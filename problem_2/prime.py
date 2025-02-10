f774afdb77e605fa3fdf467cf508bcb472388a51
JGeks <dicksonjjd@gmail.com>
2025-02-10 12:50:54 -0500

    Problem 2 - Time Exceeded

--- problem_2/prime.py
+++ problem_2/prime.py
@@ -1,4 +1,27 @@
+def prime_check(i):
+    if i > 1:
+    # Iterate from 2 to n // 2
+        for p in range(2, int(i//2)+1):  
+        # If num is divisible by any number between
+        # 2 and n / 2, it is not prime
+            if i % p == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+            
 def main():
-    pass
+    num_tests = int(input())
+    #print(f"Num tests:, {num_tests}")
+    for x in range(num_tests):
+        number_range = (input())
+        input_1, input_2 = map(int, number_range.split())
+        for i in range(input_1 , input_2 + 1):
+            if prime_check(i):
+                print(i)
+        print("")
+#2, 1 10, 3 5 
 
 main()
